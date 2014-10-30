0587821f1a704257e991c20bbcdce82babf1f261
opussf <github@zz9-za.com>
2014-10-30 11:49:53 -0700

    New API stubs for Group and Raid functions

--- src/wowStubs.lua
+++ src/wowStubs.lua
@@ -16,6 +16,9 @@ local itemDB = {
 --myInventory = { ["9999"] = 52, }
 myInventory = {}
 myCurrencies = {}
+-- set one of these to the number of people in the raid or party to reflect being in group or raid.
+-- roster should be an array for GetRaidRosterInfo
+myParty = { ["group"] = nil, ["raid"] = nil, ["roster"] = {} }
 outMail = {}
 globals = {}
 accountExpansionLevel = 4   -- 0 to 5
@@ -296,6 +299,16 @@ function GetMerchantNumItems()
 	for _ in pairs(MerchantInventory) do count = count + 1 	end
 	return count
 end
+function GetNumGroupMembers()
+	-- http://www.wowwiki.com/API_GetNumGroupMembers
+	-- Returns number of people (include self) in raid or party, 0 if not in raid / party
+	if myParty.raid then
+		return #myParty.roster
+	else
+		return #myParty.roster
+	end
+	return 0
+end
 function GetNumRoutes( nodeId )
 	-- http://wowprogramming.com/docs/api/GetNumRoutes
 	-- returns numHops
@@ -306,6 +319,14 @@ function GetNumTradeSkills( )
 	local count = 0
 	for _ in pairs( TradeSkillItems ) do count = count + 1 end
 	return count
+end
+function GetRaidRosterInfo( raidIndex )
+	-- http://www.wowwiki.com/API_GetRaidRosterInfo
+	-- returns name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML
+	if (myParty.raid or myParty.party) and myParty.roster then
+		return unpack(myParty.roster[raidIndex]) -- unpack returns the array as seperate values
+	end
+
 end
 function GetRealmName()
 	return "testRealm"
@@ -356,6 +377,13 @@ function IsInGuild()
 	-- 1, nil boolean return of being in guild
 	return 1
 end
+function IsInRaid()
+	-- http://www.wowwiki.com/API_IsInRaid
+	-- 1, nill boolean return of being in raid
+	-- myParty = { ["group"] = nil, ["raid"] = nil } -- set one of these to true to reflect being in group or raid.
+
+	return ( myParty["raid"] and 1 or nil )
+end
 function NumTaxiNodes()
 	-- http://www.wowwiki.com/API_NumTaxiNodes
 	local count = 0
@@ -377,13 +405,13 @@ function TaxiNodeCost( nodeId )
 	-- http://www.wowwiki.com/API_TaxiNodeCost
 	return TaxiNodes[nodeId].cost
 end
-function TaxiNodeGetType( nodeId )
-	-- http://www.wowwiki.com/API_TaxiNodeGetType
-	return TaxiNodes[nodeId].type
-end
 function TaxiNodeName( nodeId )
 	-- http://www.wowwiki.com/API_TaxiNodeName
 	return TaxiNodes[nodeId].name
+end
+function TaxiNodeGetType( nodeId )
+	-- http://www.wowwiki.com/API_TaxiNodeGetType
+	return TaxiNodes[nodeId].type
 end
 function UnitClass( who )
 	local unitClasses = {
