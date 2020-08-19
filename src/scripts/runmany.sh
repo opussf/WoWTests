439c0e479e34fa3743ffd5e5d4746ac318f9e106
opussf <github@zz9-za.com>
2020-08-19 13:42:27 -0700

    June 8th.  push output to target/reports.  Sleep until the test file changes
    <no random message>

--- src/scripts/runmany.sh
+++ src/scripts/runmany.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 ant clean
+mkdir -p target/reports
 for n in $(seq -f "%05g" 1 9999) ; do
 	#echo "Running run $n"
 	reportFile="target/reports/testOut.xml"
-	ant test > antout.txt
+	ant test > target/reports/antout.txt
 	if [ ! "$?" == "0" ]; then
-		mv antout.txt target/reports/antOut$n.txt
-		mv $reportFile target/reports/testOut$n.xml
+		cp target/reports/antout.txt target/reports/antOut$n.txt
+		#mv $reportFile target/reports/testOut$n.xml
 		ls -alt target/reports/testOut$n.xml
+		until $(~/Scripts/checkFileChanged.sh ./test/test.lua); do
+			sleep 1
+		done
 	else
 		ls -alt $reportFile
 	fi
