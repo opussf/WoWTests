1bb0f6b287f1d4b8d061f451eb3db4e0140c4d89
opussf <github@zz9-za.com>
2024-02-19 02:02:22 -1000

    mcheck update
    <no random message>

--- src/scripts/mcheck.sh
+++ src/scripts/mcheck.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TARGETLIMIT=$1
+
 SRCDIR="./WoWTests/target/working/WoWTests/"
 SRCFILE="wowStubs.lua"
 DIFF="diff --unchanged-line-format= --old-line-format= --new-line-format='%L'"
@@ -16,42 +18,72 @@ if [ ! -f "${SRCDIR}${SRCFILE}" ]; then
 	fi
 fi
 
-# get the md5 of the src file
-SRCFILEMD5=`md5 -q "${SRCDIR}${SRCFILE}"`
-echo "MD5 (${SRCDIR}${SRCFILE}) = $SRCFILEMD5"
+compare_files() {
+	SRCDIR=$1
+	SRCFILE=$2
+	LIMIT=$3
 
-# find all SRCFILES in subdirs, and get their md5, record to a file
-find $PWD -name $SRCFILE -exec md5 {} + > md5.out
-cat md5.out | grep -v "src" > md5.out2
+	# get the md5 of the src file
+	SRCFILEMD5=`md5 -q "${SRCDIR}${SRCFILE}"`
+	echo "MD5 (${SRCDIR}${SRCFILE}) = $SRCFILEMD5"
 
-# show the files who are different
-echo ">>> These files are different from $SRCDIR$SRCFILE"
-grep -v $SRCFILEMD5 md5.out2 > md5.out
-rm md5.out2
-cat md5.out
+	# find all SRCFILES in subdirs, and get their md5, record to a file
+	find $PWD -name $SRCFILE -exec md5 {} + > md5.out
 
-######
-echo "---------------"
-if [ -s md5.out ]; then   # md5 file exists and is non-zero size
+	if [ ! -z "$LIMIT" ]; then
+		cat md5.out | grep -v "$LIMIT" > md5.out2
+	else
+		mv md5.out md5.out2
+	fi
 
-	echo "Run: $DIFF $SRCDIR$SRCFILE"
-	while read line; do
-		# find filenames of changed files
-		CHANGEDFILE=`echo $line | grep -v $SRCFILEMD5 | cut -d'(' -f 2 | cut -d')' -f 1`
-		if [ ! -z "$CHANGEDFILE" ]; then
-			echo "	$CHANGEDFILE"
-			$DIFF $SRCDIR$SRCFILE $CHANGEDFILE
-			#echo "$?"
-		fi
-	done < md5.out
+	# show the files who are different
+	echo ">>> These files are different from $SRCDIR$SRCFILE"
+	grep -v $SRCFILEMD5 md5.out2 > md5.out
+	rm md5.out2
+	cat md5.out
 
-fi
+	######
+	echo "---------------"
+	if [ -s md5.out ]; then
+		echo "Run: $DIFF $SRCDIR$SRCFILE"
+		while read line; do
+			# find filenames of changes files
+			CHANGEDFILE=`echo $line | grep -v $SRCFILEMD5 | cut -d'(' -f 2 | cut -d')' -f 1`
+			if [ ! -z "$CHANGEDFILE" ]; then
+				echo "  $CHANGEDFILE"
+				$DIFF $SRCDIR$SRCFILE $CHANGEDFILE
+				#echo "$?"
+			fi
+		done < md5.out
+	fi
+}
+
+compare_files "./WoWTests/target/working/WoWTests/" "wowStubs.lua" "src"
+compare_files "./WoWTests/target/working/WoWTests/" "wowTest.lua" "src"
+compare_files "./WoWTests/.github/workflows/" "normal_test.yml"
+compare_files "./WoWTests/.github/workflows/" "ontag.yml"
+#compare_files "./WoWTests/buildFiles/" "build.xml"
+
 
 # these worked, sort of..
 
 #find $PWD -name $SRCFILE -ok diff --unchanged-line-format= --old-line-format= --new-line-format='%L' $SRCDIR$SRCFILE {} ';'
 #find $PWD -name $SRCFILE -ok $DIFF $SRCDIR$SRCFILE {} ';'
 #for d in `ls -d */`; do pushd $d; git st | grep -i "$SRCFILE"; popd > /dev/null ; done
+for d in *; do
+  if [ -d $d ]; then
+    pushd $d;
+    if [ -f build.xml ]; then
+        ant test > ../$d.testrun && if [ $? ]; then rm ../$d.testrun; fi &
+    fi
+    popd
+  fi
+done
+wait
+rm failed.runs
+for f in *.testrun; do
+  cat $f >> failed.runs
+done
 
 
 ##
